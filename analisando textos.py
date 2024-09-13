c890d79903f6c4f40f212ace6cc4c679f43e2e2a
Brenda Rafaele Soares Da Silva <103004471+Lovelyandyou@users.noreply.github.com>
2024-09-13 16:06:09 -0300

    Add files via upload

---
+++ analisando textos.py
@@ -0,0 +1,8 @@
+nome = str(input('Digite seu nome Completo:  ')).strip()
+print('Analisando seu nome....')
+print('Seu nome em maiúscula é {}'.format(nome.upper()))
+print('Seu nome em minúscula é {}'.format(nome.lower()))
+print('Seu nome tem ao todo {} letras'.format(len(nome) -nome.count(' ')))
+#print('Seu nome tem ao todo {} letras'.format(nome.find(' ')))
+separa = nome.split()
+print('Seu primeiro nome é {} e ele tem {} letras'.format(separa[8], len(separa[8])))
---
+++ seno cosseno e tangente.py
@@ -0,0 +1,9 @@
+import math
+
+angulo = float(input('Digite i Ângulo que você deseja: '))
+seno = math.sin(math.radians(angulo))
+print('O angulo de {} tem o seno de {:.2f}'.format(angulo, seno))
+cosseno = math.cos(math.radians(angulo))
+print('O angulo de {} tem o cosseno de {:.2f}'.format(angulo, cosseno))
+tangente = math.tan(math.radians(angulo))
+print('O angulo de {} tem o tangente de {:.2f}'.format(angulo, tangente))
